b460d3b70bae209300dcd03f4690fed9ac8474ea
aksharbarot <axar1990@gmail.com>
2021-02-23 23:04:55 +0530

    Update migrateaz.sh

--- migrateaz.sh
+++ migrateaz.sh
@@ -51,7 +51,7 @@ echo -en "${Yellow}Do you want to deploy EC2 in ($daz) AZ? (yes/no):${NOCOLOR}"
 read choice
 
 if [ "$choice" ==  "yes" ] ; then
-        AWS_PROFILE=myaccesskey ansible-playbook ec2azmigration.yaml -i localhost, --extra-vars "tag1=$ename tag2=$aname tag3=$aoname tag4=$envname tag5=$bowner tag6=$iown tag7=$bunit tag8=$steam tag9=$bill tag10=$osname tag11=$dname tag12=$sapp tag13=$bcon tag14=$depa tag15=$ccenter ecid=$eid moffering=$oid ezone=$daz zsubnet=$dsub" -vvv
+        AWS_PROFILE=myaccesskey ansible-playbook ec2azmigrate.yaml -i localhost, --extra-vars "tag1=$ename tag2=$aname tag3=$aoname tag4=$envname tag5=$bowner tag6=$iown tag7=$bunit tag8=$steam tag9=$bill tag10=$osname tag11=$dname tag12=$sapp tag13=$bcon tag14=$depa tag15=$ccenter ecid=$eid moffering=$oid ezone=$daz zsubnet=$dsub" -vvv
 else
         echo ""
         echo -e "${Cyan}Selected No. Bye!!!!!!${NOCOLOR}"
